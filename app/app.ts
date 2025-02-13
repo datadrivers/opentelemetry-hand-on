23851912b58468704ec9b4bba239a73f40bb617a
gretel <code@jitter.eu>
2025-02-13 14:40:49 +0100

    add a route to result in a dice roll by fake person (which we can eventually identify personally later)

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import { trace } from '@opentelemetry/api';
+import { faker } from '@faker-js/faker';
+
 
 const PORT: number = parseInt(process.env.PORT || '8080');
 const app: Express = express();
@@ -20,6 +22,20 @@ app.get('/rolldice', (req, res) => {
     console.log(`Rolled a ${result}`);
 
     res.send(result.toString());
+});
+
+app.get('/rollrick', (req, res) => {
+    console.log('Faking rick...');
+
+    const span = tracer.startSpan('roll-rick'); // Start a trace span
+    const randonNumber = getRandomNumber(1, 6);
+    const randomName = faker.person.fullName(); 
+    const randomEmail = faker.internet.email();
+    const result = `Rolled a ${randonNumber} for ${randomName} at ${randomEmail}`
+    span.end(); // End the trace span
+
+    console.log(result);
+    res.send(result);
 });
 
 app.listen(PORT, () => {
